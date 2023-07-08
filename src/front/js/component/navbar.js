a556b49ff36aae59196f4877257b81dc5a70a17a
danielmoret <dmoret17@gmail.com>
2023-07-08 14:07:57 -0400

    navbar done

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
-};
+  return (
+    <nav className="border-b-2 border-b-black">
+      <div className="flex justify-center py-8 divide-x">
+        <Link to="/" className="font-newyorkfont text-5xl">
+          The Daniel Times
+        </Link>
+      </div>
+    </nav>
+  )
+}
