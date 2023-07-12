4539749cf8f88fbcd3f607526277b1e6912d7c24
danielmoret <dmoret17@gmail.com>
2023-07-11 20:08:16 -0400

    add endpoint GET /news

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, News, Category
 from api.utils import generate_sitemap, APIException
+from sqlalchemy import and_, or_
 
 api = Blueprint('api', __name__)
 
@@ -35,4 +36,26 @@ def handle_news():
         except Exception as error:
             db.session.rollback()
             return jsonify({"msg": f"{error}"}), 500
-       
+
+@api.route('/news', methods=['GET'])
+def get_news():
+    queryCategory =  request.args.get("category")
+    queryTitle = f'%{request.args.get("title","")}%'
+    queryContent = f'%{request.args.get("content","")}%'
+
+    query = News.query.filter(
+        News.title.like(queryTitle),
+        News.content.like(queryContent)
+    )
+
+    if queryCategory:
+        if queryCategory not in Category.__members__:
+            raise APIException('Category not valid', status_code=400)
+        else:    
+            query = query.filter(News.category == queryCategory)
+
+    query_filter = query.all()
+    
+
+    all_news = list(map(lambda news: news.serialize(), query_filter))
+    return jsonify(all_news), 200
