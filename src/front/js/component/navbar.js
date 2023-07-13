9b9bd7da33218a4230c95e07d39a2aa49aa12ad4
danielmoret <dmoret17@gmail.com>
2023-07-13 09:23:50 -0400

    add navbar tabs

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,6 +9,29 @@ export const Navbar = () => {
           The Daniel Times
         </Link>
       </div>
+      <div class="flex justify-around font-semibold text-gray-400 ">
+        <button id="buss" class="tab-btn active hover:text-neutral-800">
+          BUSSINES
+        </button>
+        <button id="actualidad" class="tab-btn hover:text-neutral-800">
+          ENTERTEINMENT
+        </button>
+        <button id="salud" class="tab-btn hover:text-neutral-800">
+          GENERAL
+        </button>
+        <button id="economia" class="tab-btn hover:text-neutral-800">
+          HEALTH
+        </button>
+        <button id="economia" class="tab-btn hover:text-neutral-800">
+          SCIENCE
+        </button>
+        <button id="economia" class="tab-btn hover:text-neutral-800">
+          SPORTS
+        </button>
+        <button id="economia" class="tab-btn hover:text-neutral-800">
+          TECHNOLOGY
+        </button>
+      </div>
     </nav>
   )
 }
