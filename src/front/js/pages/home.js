1b3b9d451ba2970ac47cb4b9fc005c4740e323b6
danielmoret <dmoret17@gmail.com>
2023-07-13 23:07:24 -0400

    add search svg

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import { Context } from '../store/appContext'
 import rigoImageUrl from '../../img/rigo-baby.jpg'
 import '../../styles/index.css'
+import Search from '../component/Search.jsx'
 
 export const Home = () => {
   const { store, actions } = useContext(Context)
 
   return (
     <div>
+      <Search />
       <h1>Home</h1>
     </div>
   )
