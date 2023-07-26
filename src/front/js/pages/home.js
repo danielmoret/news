0bc1fe6bd25013efc5c97204e0d81f7f07ac22ca
danielmoret <dmoret17@gmail.com>
2023-07-25 22:43:29 -0400

    fetch and map news cards

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,13 +2,18 @@ import React, { useContext } from 'react'
 import { Context } from '../store/appContext'
 import rigoImageUrl from '../../img/rigo-baby.jpg'
 import '../../styles/index.css'
+import Cards from '../component/cards'
 
 export const Home = () => {
   const { store, actions } = useContext(Context)
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="mt-5">
+      <div className="flex justify-center flex-wrap gap-2">
+        {store.news.map((item) => {
+          return <Cards key={item.id} news={item} />
+        })}
+      </div>
     </div>
   )
 }
